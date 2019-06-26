c3abbc06e8621efd656e3aee8ea9f8269f856a46
sean <mkwak@my.centennialcollege.ca>
2019-06-26 15:35:13 -0400

    added infoLabel

--- COMP123-S2019-LESSION8B/Lab8Form.Designer.cs
+++ COMP123-S2019-LESSION8B/Lab8Form.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.TitleLabel08 = new System.Windows.Forms.Label();
+            this.infoGroupBox = new System.Windows.Forms.GroupBox();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.infoGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // TitleLabel08
@@ -41,16 +45,49 @@
             this.TitleLabel08.TabIndex = 0;
             this.TitleLabel08.Text = "Lab08";
             // 
+            // infoGroupBox
+            // 
+            this.infoGroupBox.Controls.Add(this.textBox1);
+            this.infoGroupBox.Controls.Add(this.nameLabel);
+            this.infoGroupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.infoGroupBox.Location = new System.Drawing.Point(13, 49);
+            this.infoGroupBox.Name = "infoGroupBox";
+            this.infoGroupBox.Size = new System.Drawing.Size(599, 244);
+            this.infoGroupBox.TabIndex = 1;
+            this.infoGroupBox.TabStop = false;
+            this.infoGroupBox.Text = "Info";
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(6, 23);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(177, 31);
+            this.nameLabel.TabIndex = 0;
+            this.nameLabel.Text = "Enter Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(189, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(404, 38);
+            this.textBox1.TabIndex = 1;
+            // 
             // Lab8
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.infoGroupBox);
             this.Controls.Add(this.TitleLabel08);
             this.Name = "Lab8";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "lab8Form";
+            this.infoGroupBox.ResumeLayout(false);
+            this.infoGroupBox.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,6 +96,9 @@
         #endregion
 
         private System.Windows.Forms.Label TitleLabel08;
+        private System.Windows.Forms.GroupBox infoGroupBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label nameLabel;
     }
 }
 
